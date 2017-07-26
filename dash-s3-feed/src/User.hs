c905b3157481bd6ddfbdc589755a830bcf6b7be4
Dan Fithian <dan.fithian@tvisioninsights.com>
2017-07-26 12:38:29 -0400

    Add password validation and a way to insert passwords

--- dash-s3-feed/src/User.hs
+++ dash-s3-feed/src/User.hs
@@ -1,8 +1,10 @@
 module User where
 
 import ClassyPrelude
-import Composite.Record ((:->)(Val))
+import Composite.Record ((:->)(Val), Record)
+import Crypto.KDF.BCrypt (validatePassword)
 import Control.Lens (view)
+import Data.Vinyl.Lens (rcast)
 import Opaleye (runQuery)
 import Servant (BasicAuthData(BasicAuthData))
 import Servant.Server
@@ -12,7 +14,7 @@ import Servant.Server
 
 import Foundation (App, runDb)
 import Model (selectUser)
-import Types (UserJson(UserJson), fPassword)
+import Types (User, UserJson(UserJson), fPassword)
 
 basicAuthContext :: App -> Context (BasicAuthCheck UserJson ': '[])
 basicAuthContext app = authCheck app :. EmptyContext
@@ -22,5 +24,7 @@ authCheck app = BasicAuthCheck $ \ (BasicAuthData name password) -> do
   users <- runReaderT (runDb . flip runQuery $ selectUser (Val name)) app
   case headMay users of
     Nothing -> pure NoSuchUser
-    Just badUser | view fPassword badUser /= password -> pure BadPassword
-    Just user -> pure $ Authorized $ UserJson user
+    Just user -> do
+      case validatePassword password $ view fPassword user of
+        True -> pure . Authorized . UserJson . rcast $ (user :: Record User)
+        False -> pure BadPassword
